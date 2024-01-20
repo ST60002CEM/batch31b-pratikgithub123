eac6298409aad9fad9d2763df7d1b6218501228e
pratikgithub123 <dangipratik19@gmail.com>
2024-01-20 20:18:31 +0545

    api model created

---
+++ lib/features/products/data/model/products_api_model.dart
@@ -0,0 +1,28 @@
+class Product {
+  final String productName;
+  final double productPrice;
+  final String productDescription;
+  final String productCategory;
+  final String productImageUrl;
+  final DateTime createdAt;
+
+  Product({
+    required this.productName,
+    required this.productPrice,
+    required this.productDescription,
+    required this.productCategory,
+    required this.productImageUrl,
+    required this.createdAt,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      productName: json['productName'],
+      productPrice: json['productPrice'].toDouble(),
+      productDescription: json['productDescription'],
+      productCategory: json['productCategory'],
+      productImageUrl: json['productImageUrl'],
+      createdAt: DateTime.parse(json['createdAt']),
+    );
+  }
+}
